b1c42765241bcdeda7543a6312591e7f67b43d56
scottdernley <scott@tractor.finance>
2022-02-10 09:49:14 -0800

    Added BTTWO

---
+++ subgraphs/bttwo/mappings/mapping.ts
@@ -0,0 +1,15 @@
+import { Tx } from "../generated/schema";
+import { Transfer } from "../generated/bttwo/abi";
+
+
+export function handleTransfer(event: Transfer): void {
+
+  let txId = event.transaction.hash.toHex()
+  let tran = new Tx(txId)
+  tran.to = event.params.to
+  tran.from = event.params.from
+  tran.value = event.params.value
+  tran.block = event.block.number
+  tran.save()
+
+}
